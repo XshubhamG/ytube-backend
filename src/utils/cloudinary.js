03ba09b6861fb11eb345168e06a843c7f6606fc6
Shubham Giri <shubhammgiri3@gmail.com>
2025-11-20 10:44:29 +0530

    configured cloudinary and multer middleware for file uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecretKey = process.env.CLOUDINARY_API_SECRET_KEY;
+
+cloudinary.config({
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecretKey,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("file upload successful", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
